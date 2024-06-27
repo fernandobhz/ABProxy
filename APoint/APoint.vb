cb22a8a929e5267a986f486ed01a9f83d32641b2
Fernando Reis Guimaraes <fernandobhz@gmail.com>
2024-06-27 18:08:56 -0300

    fix: better behavior descriptions

--- APoint/APoint.vb
+++ APoint/APoint.vb
@@ -7,19 +7,29 @@ Public Module APoint
     Sub Main()
         Dim Path = System.Reflection.Assembly.GetExecutingAssembly().Location
         Dim Params = System.IO.Path.GetFileNameWithoutExtension(Path)
-        If Not Params.Contains("@") Then Params = InputBox("Parametros", "Parametros", "3264@32128")
+
+        If Params.StartsWith("APoint") Then
+            Params = Mid(Params, "APoint".Length + 1)
+        End If
+
+        If Not Params.Contains("@") Then Params = InputBox(
+            "You can define the params the filename.exe" & vbCrLf & vbCrLf &
+            "Example: 80@8080" & vbCrLf & vbCrLf &
+            "This will expose the port 80 to the Web" & vbCrLf & vbCrLf &
+            "While waiting proxy connections from port 8080", "Params: ExposedToWoldWideWebPort @ ProxyPort", "80@8080")
+
         Dim Matches = Regex.Matches(Params, "([0-9]+)[@]([0-9]+)") : If Matches.Count = 0 Then Exit Sub
 
-        Dim ClientPort = Matches(0).Groups(1).Value
-        Dim APort = Matches(0).Groups(2).Value
+        Dim ExposedToWoldWideWebPort = Matches(0).Groups(1).Value
+        Dim ProxyPort = Matches(0).Groups(2).Value
 
         Dim ABConnection As TcpClient = Nothing
 
         Do
             Try
                 If ABConnection Is Nothing OrElse Not ABConnection.Connected Then
-                    ABConnection = Tcp.Wait(APort)
-                    TcpProxy.Open(Tcp.Wait(ClientPort), ABConnection)
+                    ABConnection = Tcp.Wait(ProxyPort)
+                    TcpProxy.Open(Tcp.Wait(ExposedToWoldWideWebPort), ABConnection)
                 Else
                     Thread.Sleep(100)
                 End If
